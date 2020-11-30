857be2521d95bd55e102c7cae06f8bb2b2b82eeb
kraftdev <kraftd1@go.stockton.edu>
2020-11-29 20:14:03 -0500

    Revert "Oops, we didn't want this commit"
    This reverts commit c812b64cd39ee4640deaf40ba709e00bd499dc80.

--- Hello.java
+++ Hello.java
@@ -1,13 +1,9 @@
 ﻿public class Hello
-{
- public static void main( String argv[] )
- {
- // This is an unwanted but staged comment
- String name = "World";
- if ( argv.length != 0 )
- {
- name = argv[0];
- }
- System.out.println( "Hello, " + name + "!" );
- }
+{ 
+	String name = "World";
+	if ( argv.length != 0 )
+ 	{
+		name = argv[0];
+	}
+	System.out.println( "Hello, " + name + "!" );
 }
