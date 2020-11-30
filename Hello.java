c812b64cd39ee4640deaf40ba709e00bd499dc80
kraftdev <kraftd1@go.stockton.edu>
2020-11-29 20:13:53 -0500

    Oops, we didn't want this commit

--- Hello.java
+++ Hello.java
@@ -1,9 +1,13 @@
 ﻿public class Hello
-{ 
-	String name = "World";
-	if ( argv.length != 0 )
- 	{
-		name = argv[0];
-	}
-	System.out.println( "Hello, " + name + "!" );
+{
+ public static void main( String argv[] )
+ {
+ // This is an unwanted but staged comment
+ String name = "World";
+ if ( argv.length != 0 )
+ {
+ name = argv[0];
+ }
+ System.out.println( "Hello, " + name + "!" );
+ }
 }
