d6e732e837281d5d930d71bc2079262186f89676
kraftdev <kraftd1@go.stockton.edu>
2020-11-29 20:04:03 -0500

    Added a defult value

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
 ﻿public class Hello
-	{ 
-	public static void main( String argv[] )
-	{
- 		System.out.println( "Hello, World" );
+{ 
+	String name = "World";
+	if ( argv.length != 0 )
+ 	{
+		name = argv[0];
 	}
-	}
+	System.out.println( "Hello, " + name + "!" );
+}
